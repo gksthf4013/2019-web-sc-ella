cd57fdebf3de07aa010a1dd626a55bdf14027b20
gksthf4013 <gksthf4013@naver.com>
2019-10-22 13:19:15 +0900

    slide 11 완성

--- public/js/slide6.js
+++ public/js/slide6.js
@@ -1,34 +1,54 @@
-(function(){
-	var now = 1;
-	var gap = 2000;
-	var speed = 500;
-	var ani = function(){
-		$(".banners").stop().animate({"top": (-380*now)+"px"}, speed, function(){
-			if(now == 5) {
-				now = 1;
-				$(this).css({"top": 0});
-			}
-			else now++;
-		});
-	}
-	var interval = setInterval(ani, gap);
+// 전역변수
+var now = 0;
+
+// 이벤트
+$(".bt-prev").click(function(){
+	if(now > 0) now--;
+	init();
+}).hide();
+$(".bt-next").click(function(){
+	if(now < 4) now++;
+	init();
+});
+$(".pager").click(function(){
+	now = $(this).index();
+	init();
+});
+
+/* 동작 */
+// 시작할 때 한번 실행
+(function () {
+	pagerInit();
 })();
-
-
-/* 
-var now = 1;
-var interval = setInterval(slide, 3000);
-function slide(){
-	$(".banners").stop().animate({
-		"top": (-380*now) + "px"
-	}, 500, function(){
-		if(now == 5) {
-			now = 1;
-			$(this).css({
-				"top": 0
-			});
-		}
-		else now++;
-	});
+// 이벤트 발생할 때 실행할 함수
+function init() {
+	ani();
+	btInit();
+	pagerInit();
 }
- */
+// 애니메이션
+function ani() {
+	$(".banners").stop().animate({"top": (-380*now)+"px"}, 500);
+}
+// 버튼 정렬
+function btInit() {
+	if(now == 0) {
+		$(".bt-prev").hide();
+		$(".bt-next").show();
+	}
+	else if(now == 4) {
+		$(".bt-prev").show();
+		$(".bt-next").hide();
+	}
+	else {
+		$(".bt-prev").show();
+		$(".bt-next").show();
+	}
+}
+// 페이저 정렬
+function pagerInit() {
+	$(".pager").removeClass("active");
+	$(".pager").eq(now).addClass("active");
+}
+
+
