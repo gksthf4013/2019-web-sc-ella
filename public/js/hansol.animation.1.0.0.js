e148ae35293c15b41e72b06ae4aacb76eb063ef2
gksthf4013 <gksthf4013@naver.com>
2019-10-23 15:15:42 +0900

    hansol.animation

---
+++ public/js/hansol.animation.1.0.0.js
@@ -0,0 +1,53 @@
+var AniEasy = (function(){
+	function AniEasy(obj) {
+		this.elem = obj.elem == undefined ? ".ani-easy" : obj.elem;
+		this.elem = document.querySelector(this.elem);
+		this.interval;
+	}
+	return AniEasy;
+})();
+
+AniEasy.prototype.animate = function(css, speed, cb) {
+	this.css = css;
+	this.speed = speed;
+	this.cb = (typeof cb === 'undefined') ? false : (typeof cb === 'function') ? cb : false;
+	for(var key in this.css) {
+		this.cssName = key;
+		this.cssValue = this.css[key];
+	}
+	var pos = Number(this.cssValue.replace("px", ""));
+	var tar = Number(getComputedStyle(this.elem)[this.cssName].replace("px", ""));
+	clearInterval(this.interval);
+	this.interval = setInterval(ani, 20, this);
+	function ani(obj) {
+		if(Math.ceil(Math.abs(tar)) == Math.abs(pos)) {
+			clearInterval(obj.interval);
+			obj.elem.style[obj.cssName] = pos + "px";
+			if(obj.cb) obj.cb();
+		}
+		else {
+			tar += (pos - tar) * 0.15;
+			obj.elem.style[obj.cssName] = tar + "px";
+		}
+	}
+}
+
+
+/*  위의 코딩의 정석의 방법
+var AniEasy = (function(){
+	function AniEasy(obj) {
+		this.elem = (typeof obj.elem == 'undefined') ? ".ani-easy" : obj.elem;
+		this.target = obj.target;
+		this.speed = (typeof obj.speed == 'undefined') ? 500 : Number(obj.speed);
+	}
+	return AniEasy;
+})();
+ */
+
+/* 
+obj = {
+	elem: ".box", "#box", "div",
+	target: 700,
+	speed: 500,
+}
+*/
--- public/js/slide12.js
+++ public/js/slide12.js
@@ -6,6 +6,7 @@ var delay = 2000;
 var cnt = 0;	// Ajax으로 가져온 슬라이드 갯수
 var html = ["", ""];
 var ajax = new XMLHttpRequest();
+var aniEasy = new AniEasy({elem: ".banners"});
 
 // 시작
 ajax.onreadystatechange = slideInit; // CallBack Init
@@ -80,10 +81,10 @@ function init() {
 }
 // 애니메이션
 function ani() {
-	$(".banners").stop().animate({"left": (-720*now)+"px"}, 500, function(){
+	aniEasy.animate({"left": (-720*now)+"px"}, 500, function(){
 		if(now == 5) {
 			now = 0;
-			$(".banners").css("left", 0);
+			document.querySelector(".banners").style.left = 0;
 			pagerInit();
 			btInit();
 		}
