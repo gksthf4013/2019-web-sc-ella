92c249862a3a1b0e25f77162f12e58fcf4debec7
gksthf4013 <gksthf4013@naver.com>
2019-10-28 11:57:33 +0900

    19.video-youtube.html

--- public/js/scroll-wheel.js
+++ public/js/scroll-wheel.js
@@ -1,27 +1,24 @@
-window.addEventListener("wheel", function(){event.preventDefault();}, {passive: false});
-document.querySelectorAll(".page").forEach(function(page, key) {
-	page.addEventListener("wheel", wheelFn, {passive: false});
-});
-
+window.addEventListener("wheel", wheelFn, {passive: false});
 function wheelFn(){
 	event.preventDefault();
 	event.stopPropagation();
-	var hei = $(this).outerHeight();
+	var hei = $(window).outerHeight();
 	var delta = event.deltaY;
 	var posY = event.pageY;
-	var n = Math.floor(posY/hei); // index값
-	var pageCnt = $(".page").length; // 4
-	if(delta > 0) {
-		// 아래로 휠
-		if(n < pageCnt - 1) wheelAni((n + 1) * hei);
-	}
-	else {
-		// 위로 휠
-		if(n > 0) wheelAni((n - 1) * hei);
-	}
+	var n = Math.floor(posY/hei);
+	var pageCnt = $(".page").length;	//4
+	if(delta > 0) (n < pageCnt - 1) ? wheelAni((n + 1) * hei, n, (n + 1)) : 0;	// 아래로 휠
+	else (n > 0) ? wheelAni((n - 1) * hei, n, (n - 1)) : 0;	// 위로 휠
 }
-function wheelAni(pos) {
-	$("html, body").stop().animate({
-		"scrollTop": pos
-	}, 300);
+function wheelAni(pos, prevPage, nowPage) {
+	console.log(prevPage, nowPage);
+	$("html, body").stop().animate({"scrollTop": pos}, 1000, function(){
+
+	});
 }
+
+$(".navi").click(function(){
+	var n = $(this).index();
+	var hei = $(window).outerHeight();
+	wheelAni(n * hei);
+});
