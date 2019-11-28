8ee26279cc0b2922c554590e9848085506cda8ce
gksthf4013 <gksthf4013@naver.com>
2019-11-28 16:45:44 +0900

    수정구문

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ const util = require(path.join(__dirname, "modules/util"));
 app.locals.pretty = true;
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
@@ -39,12 +37,8 @@ app.use(methodOverride(function (req, res) {
 }));
 
 /* morgan 설정 */
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'log/access.log'), {
-	flags: 'a'
-});
-app.use(morgan('combined', {
-	stream: accessLogStream
-}));
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'log/access.log'), {flags: 'a'});
+app.use(morgan('combined', { stream: accessLogStream }));
 
 
 /* router - ella */
@@ -68,17 +62,26 @@ app.use("/rest-seq", seqRouter);
 */
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+
+
+
+
+
+
+
+
--- router/admin-banner.js
+++ router/admin-banner.js
@@ -2,42 +2,34 @@ const express = require("express");
 const router = express.Router();
 const mt = require("../modules/multer-conn");
 const path = require("path");
-const {
-	AdminBanner
-} = require(path.join(__dirname, "../model/AdminBanner"));
+const { AdminBanner } = require(path.join(__dirname, "../model/AdminBanner"));
 const util = require(path.join(__dirname, "../modules/util"));
 
 /* REST */
 router.get("/:type", getData);
 router.post("/:type", mt.upload.single("src"), postData);
 router.delete("/:type", deleteData);
+router.put("/:type", putData);
 
 /* Router CB */
 async function getData(req, res, next) {
 	let type = req.params.type;
 	let vals = {
-		leftNavs: [{
-				link: "/admin/banner/top",
-				title: "상단 배너 관리"
-			},
-			{
-				link: "/admin/banner/bottom",
-				title: "하단 배너 관리"
-			},
+		leftNavs: [
+			{link: "/admin/banner/top", title: "상단 배너 관리"},
+			{link: "/admin/banner/bottom", title: "하단 배너 관리"},
 		],
 	}
-	switch (type) {
+	switch(type) {
 		case "top":
 			let result;
-			if (req.query.id) {
-				result = await AdminBanner.findOne({where: { id : req.query.id }});
+			if(req.query.id) {
+				result = await AdminBanner.findOne({ where: { id : req.query.id } });
 				res.json(result);
-			} 
+			}
 			else {
-				let result = await AdminBanner.findAll({
-					order: [
-						["id", "desc"]
-					],
+				result = await AdminBanner.findAll({
+					order: [["id", "desc"]],
 				});
 				vals.lists = result;
 				res.render("admin/bannerTop", vals);
@@ -59,30 +51,27 @@ async function postData(req, res, next) {
 	let link = req.body.link;
 	let desc = req.body.desc;
 	let src = "";
-	if (req.file) src = req.file.filename;
+	if(req.file) src = req.file.filename;
 	let result = await AdminBanner.create({
-		title,
-		position,
-		link,
-		desc,
-		src
+		title, position, link, desc, src
 	});
-	res.redirect("/admin/banner/" + type);
+	res.redirect("/admin/banner/"+type);
 }
 
 async function deleteData(req, res, next) {
 	let type = req.params.type;
 	let id = req.body.id;
 	try {
-		let result = await AdminBanner.destroy({
-			where: {
-				id
-			}
-		});
-		res.redirect('/admin/banner/' + type);
-	} catch (error) {
+		let result = await AdminBanner.destroy({ where: { id } });
+		res.redirect('/admin/banner/'+type);
+	}
+	catch(error) {
 		next(error);
 	}
+}
+
+async function putData(req, res, next) {
+	res.send('저장되었습니다.');
 }
 
 
